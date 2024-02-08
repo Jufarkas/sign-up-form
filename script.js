124ada6c4c0d8570005b8e38b96de2cf7231848e
Jufarkas <139999007+Jufarkas@users.noreply.github.com>
2024-02-08 13:39:49 -0800

    add function to watch for password update if initially incorrect

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ let submit = document.querySelector('.createAccount');
 let pass = document.getElementById('password');
 let passConfirm = document.getElementById('passConfirm');
 let form = document.querySelector('form');
-let input = document.querySelectorAll('input');
+let formInput = document.querySelectorAll('input');
 
 // stop resubmission on refresh
 if ( window.history.replaceState ) {
     window.history.replaceState( null, null, window.location.href );
 }
-
 
 // confirm passwords match
 
@@ -18,7 +17,20 @@ form.addEventListener('submit', (e) => {
         passConfirm.setCustomValidity("Passwords do not match");
         passConfirm.reportValidity();
         passConfirm.classList.add('invalid');
+        passConfirm.addEventListener('keyup', () => {
+            validatePassword();
+        })
+    } else {
+        passConfirm.setCustomValidity('');
+        passConfirm.classList.remove('invalid');
+    }
+});
+
+function validatePassword(){
+    if(pass.value != passConfirm.value) {
+        passConfirm.setCustomValidity("Passwords Don't Match");
     } else {
         passConfirm.setCustomValidity('');
     }
-})
+};
+
